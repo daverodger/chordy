6468553cd9e36d9b4cb219b7c79d22c98f0b8731
balthasar <rodgerdave@proton.me>
2024-07-10 21:51:04 -0700

    add find_progression method; tests

--- src/helpers.rs
+++ src/helpers.rs
@@ -15,3 +15,34 @@ pub fn is_roman_tone(c: char) -> bool {
         _ => false
     }
 }
+
+pub fn find_progression(progressions: Vec<GenericProgression>, name: &str) -> Option<GenericProgression> {
+    for p  in progressions {
+        if p.name.eq_ignore_ascii_case(name) {
+            return Some(p)
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn interval_solver() {
+        let note = Note::new('D', Pitch::Natural);
+
+        let five = ScaleTone::Five;
+        let new = solve_interval(note.clone(), five);
+        assert_eq!(new, Note::new('A', Pitch::Natural));
+
+        let flat_two = ScaleTone::FlatTwo;
+        let new = solve_interval(note.clone(), flat_two);
+        assert_eq!(new, Note::new('D', Pitch::Sharp));
+
+        let seven = ScaleTone::Seven;
+        let new = solve_interval(note.clone(), seven);
+        assert_eq!(new, Note::new('C', Pitch::Sharp));
+    }
+}
